7a5df9b42c52a39a42d7033f894ab8ab03ca5db8
Sinem Karaemin <sinemzkaraemin@gmail.com>
2023-08-26 19:30:01 +0300

    Music App
    This is an app that allows a user to browse different albums, including the author, genre, and price. The user can create a
    catalog of albums. Album creators can also edit or delete their posts at any time.

---
+++ preparation_project_web_basics/preparation_project_web_basics/web/templatetags/tags.py
@@ -0,0 +1,16 @@
+from django import template
+
+from preparation_project_web_basics.web.models import Profile
+
+register = template.Library()
+
+
+@register.inclusion_tag(
+    'web/tags/navigation.html',
+    name='custom_navigation'
+)
+def inclusion_custom_navigation():
+    inner_context = {
+        "profile": Profile.objects.first()
+    }
+    return inner_context
---
+++ preparation_project_web_basics/preparation_project_web_basics/web/validators.py
@@ -0,0 +1,42 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+
+
+def max_length_validator(value):
+    max_length = 15
+    if len(value) > max_length:
+        raise ValidationError(f'Ensure this value has at most {max_length} characters.')
+
+
+@deconstructible
+class MaxLengthClassValidator:
+    def __init__(self, max_length):
+        self.max_length = max_length
+
+    def __call__(self, value):
+        if len(value) > self.max_length:
+            raise ValidationError(f'Ensure this value has at most {self.max_length} characters.')
+
+    def __eq__(self, other):
+        return self.max_length == other.max_length
+
+
+@deconstructible
+class TextContainsOnlyAlphaNumericAndUnderscoreValidator:
+    def __call__(self, value):
+        for char in value:
+            if not (char.isalnum() or char == '_'):
+                raise ValidationError('Ensure this value contains only letters, numbers, and underscore.')
+
+    def __eq__(self, other):
+        return True
+
+
+@deconstructible
+class CustomFloatPositiveValidator:
+    def __call__(self, value):
+        if value < 0.0:
+            raise ValidationError('Ensure this value is greater than or equal to 0.0.')
+
+    def __eq__(self, other):
+        return True
